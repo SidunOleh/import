9faf170afb3630493c2e17dfb706a37b0b647af5
Oleh Sidun <sidunoleh@gmail.com>
2024-01-28 10:21:49 +0200

    fix

--- import.php
+++ import.php
@@ -10,7 +10,6 @@ use Import\PostType;
 use Import\Taxonomy;
 use Carbon_Fields\Container;
 use Carbon_Fields\Field;
-use Import\Event;
 use Import\Importer\ImporterFactory;
 
 defined('ABSPATH') or die;
