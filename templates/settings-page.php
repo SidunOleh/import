8523da8b59087aacf3ace20682ce0fbd2d42a3ea
Oleh Sidun <sidunoleh@gmail.com>
2024-03-17 05:07:49 +0200

    set time limit

--- templates/settings-page.php
+++ templates/settings-page.php
@@ -38,37 +38,37 @@ $locations = get_terms( [
             <br>
             <br>
             <select name="settings[templates][<?php echo $i ?>][rule][cuisine]">
-                    <option selected disabled>
-                        <?php _e('Select cuisine') ?>
-                    </option>
-                    <option 
-                        value="*"
-                        <?php echo '*' == ($template['rule']['cuisine'] ?? '') ? 'selected' : '' ?>>
-                        <?php _e('All') ?>
-                    </option>
+                <option selected disabled>
+                    <?php _e('Select cuisine') ?>
+                </option>
+                <option 
+                    value="*"
+                    <?php echo '*' == ($template['rule']['cuisine'] ?? '') ? 'selected' : '' ?>>
+                    <?php _e('All') ?>
+                </option>
                 <?php foreach ($cuisines as $cuisine): ?>
-                    <option 
-                        value="<?php echo $cuisine->slug ?>" 
-                        <?php echo urldecode($cuisine->slug) == urldecode($template['rule']['cuisine'] ?? '') ? 'selected' : '' ?>>
-                        <?php echo $cuisine->name ?>
-                    </option>
+                <option 
+                    value="<?php echo $cuisine->slug ?>" 
+                    <?php echo urldecode($cuisine->slug) == urldecode($template['rule']['cuisine'] ?? '') ? 'selected' : '' ?>>
+                    <?php echo $cuisine->name ?>
+                </option>
                 <?php endforeach ?>
             </select>
             <select name="settings[templates][<?php echo $i ?>][rule][location]">
-                    <option selected disabled>
-                        <?php _e('Select location') ?>
-                    </option>
-                    <option 
-                        value="*"
-                        <?php echo '*' == ($template['rule']['location'] ?? '') ? 'selected' : '' ?>>
-                        <?php _e('All') ?>
-                    </option>
+                <option selected disabled>
+                    <?php _e('Select location') ?>
+                </option>
+                <option 
+                    value="*"
+                    <?php echo '*' == ($template['rule']['location'] ?? '') ? 'selected' : '' ?>>
+                    <?php _e('All') ?>
+                </option>
                 <?php foreach ($locations as $location): ?>
-                    <option 
-                        value="<?php echo $location->slug ?>"
-                        <?php echo urldecode($location->slug) == urldecode($template['rule']['location'] ?? '') ? 'selected' : '' ?>>
-                        <?php echo $location->name ?>
-                    </option>
+                <option 
+                    value="<?php echo $location->slug ?>"
+                    <?php echo urldecode($location->slug) == urldecode($template['rule']['location'] ?? '') ? 'selected' : '' ?>>
+                    <?php echo $location->name ?>
+                </option>
                 <?php endforeach ?>
             </select>
             <?php echo wp_editor($template['text'], 'template', [
